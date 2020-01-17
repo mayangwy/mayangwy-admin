17be6fd23901e17f5f589e3ae16aaf6107915fc6
mayang@winning.com.cn <mayang@winning.com.cn>
2020-01-17 17:25:21 +0800

    代码提交

--- src/main/java/org/mayangwy/admin/server/AdminApplication.java
+++ src/main/java/org/mayangwy/admin/server/AdminApplication.java
@@ -3,16 +3,14 @@ package org.mayangwy.admin.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableAspectJAutoProxy
 @EnableTransactionManagement
-@EnableJpaRepositories(basePackages = "org.mayangwy.admin.dao")
-@EntityScan(basePackages = "org.mayangwy.admin.entity")
+@EnableJpaRepositories(basePackages = {"org.mayangwy.admin.dao"})
+@EntityScan(basePackages = {"org.mayangwy.admin.entity"})
 @SpringBootApplication(scanBasePackages = {"org.mayangwy"})
 public class AdminApplication {
 
