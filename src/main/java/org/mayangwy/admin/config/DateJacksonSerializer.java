cde2227ecb16d0329175b3acad325aec9dfb27c6
m2200 <1505054490@qq.com>
2020-01-19 01:42:07 +0800

    代码提交

--- src/main/java/org/mayangwy/admin/config/DateJacksonSerializer.java
+++ src/main/java/org/mayangwy/admin/config/DateJacksonSerializer.java
@@ -1,28 +1,28 @@
-package org.mayangwy.admin.config;
-
-import cn.hutool.core.date.DateUtil;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-
-import java.io.IOException;
-import java.util.Date;
-
-public class DateJacksonSerializer extends JsonSerializer<Date> {
-
-    private String dateFormat;
-
-    public void setDateFormat(String dateFormat) {
-        this.dateFormat = dateFormat;
-    }
-
-    @Override
-    public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        gen.writeString(DateUtil.format(value, dateFormat));
-    }
-
-    @Override
-    public Class<Date> handledType() {
-        return Date.class;
-    }
-}
+//package org.mayangwy.admin.config;
+//
+//import cn.hutool.core.date.DateUtil;
+//import com.fasterxml.jackson.core.JsonGenerator;
+//import com.fasterxml.jackson.databind.JsonSerializer;
+//import com.fasterxml.jackson.databind.SerializerProvider;
+//
+//import java.io.IOException;
+//import java.util.Date;
+//
+//public class DateJacksonSerializer extends JsonSerializer<Date> {
+//
+//    private String dateFormat;
+//
+//    public void setDateFormat(String dateFormat) {
+//        this.dateFormat = dateFormat;
+//    }
+//
+//    @Override
+//    public void serialize(Date value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+//        gen.writeString(DateUtil.format(value, dateFormat));
+//    }
+//
+//    @Override
+//    public Class<Date> handledType() {
+//        return Date.class;
+//    }
+//}
